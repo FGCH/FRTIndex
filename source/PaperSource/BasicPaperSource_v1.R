14a8a0e707e1f6717ae9e6d9f0b0ed68920a6d43
Christopher Gandrud <christopher.gandrud@gmail.com>
2014-03-23 14:35:51 +0100

    minor

--- source/PaperSource/BasicPaperSource_v1.R
+++ source/PaperSource/BasicPaperSource_v1.R
@@ -31,6 +31,10 @@ Countries <- read.csv('source/ParameterDescript/CountryNumbers.csv',
 Years <- read.csv('source/ParameterDescript/YearNumbers.csv',
                       stringsAsFactors = FALSE)
 
+Indicators <- read.csv('source/IndicatorDescript/IncludedIndicators.csv',
+                       stringsAsFactors = FALSE)
+Indicators$ID <- 1:nrow(Indicators)
+
 # --------------------------------------------------- #
 #### Build identifiers ####
 # Create country number identifier
@@ -39,9 +43,14 @@ Countries$countrynumMod <- paste0('transparency\\[', Countries$countrynum, ',.*'
 # Create year identifiers
 Years$yearnumMod <- paste0('transparency\\[.*,', Years$yearnum, '\\]')
 
+# Create difficulty identifiers
+Indicators$difficultyID <- paste0('beta', Indicators$ID, '\\[1\\]')
+
+# Create discrimination identifiers
+Indicators$discriminateID <- paste0('beta', Indicators$ID, '\\[2\\]')
+
 # --------------------------------------------------- #
 #### Transparency plots ####
-
 
 pdf(file = 'paper/figures/FRT_1998.pdf', width = 12, height = 12)
 ggs_caterpillar_label(Set, family = 'transparency.*,1\\].*',
@@ -83,5 +92,40 @@ ggs_caterpillar_label(Set, family = Hung,
                       order = FALSE) +
   xlab('FRT Index (HPD)\n') + ylab('') +
   scale_y_discrete(breaks = c(1998, 2003, 2008, 2011)) +
+  theme_bw()
+dev.off()
+
+
+#### Test ####
+TestCountry <- Countries$countrynumMod[Countries$country == 'Portugal']
+
+ggs_caterpillar_label(Set, family = TestCountry,
+                      horizontal = FALSE,
+                      param_label_from = Years$yearnumMod,
+                      param_label_to = Years$year,
+                      order = FALSE) +
+  xlab('FRT Index (HPD)\n') + ylab('') +
+  scale_y_discrete(breaks = c(1998, 2003, 2008, 2011)) +
+  theme_bw()
+
+# --------------------------------------------------- #
+#### Plot difficulty parameter ####
+
+pdf(file = 'paper/figures/difficultyPlot.pdf', width = 12)
+ggs_caterpillar_label(Set, family = 'beta.*\\[1\\].*',
+                      param_label_from = Indicators$difficultyID,
+                      param_label_to = Indicators$Indicator.Name) +
+  ylab('') + xlab('\nCoefficient') +
+  theme_bw()
+dev.off()
+
+# --------------------------------------------------- #
+#### Plot discrimination parameter ####
+
+pdf(file = 'paper/figures/discriminationPlot.pdf', width = 12)
+ggs_caterpillar_label(Set, family = 'beta.*\\[2\\].*',
+                      param_label_from = Indicators$discriminateID,
+                      param_label_to = Indicators$Indicator.Name) +
+  ylab('') + xlab('\nCoefficient') +
   theme_bw()
 dev.off()
