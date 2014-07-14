3e99ca83b5b0cd130b0535829c6572a714816c2e
Christopher Gandrud <christopher.gandrud@gmail.com>
2014-07-14 17:28:55 +0200

    minor spacing

--- source/miscFunctions/stan_catterpillar.R
+++ source/miscFunctions/stan_catterpillar.R
@@ -1,35 +1,35 @@
 #' A function to create catterpillar plots from stanfit objects from rstan
-#' 
+#'
 #' @param obj a \code{stanfit} object
 #' @param params character string, vector, or regular expression of paramater
-#' labels that you would like to plot as declared in \code{model_code} from the 
+#' labels that you would like to plot as declared in \code{model_code} from the
 #' \code{\link{stan}} call.
 #' @param params_labels vector of parameter labels. Important: they must be in
 #' the same order as in the \code{stanfit} object when \code{as.data.frame(obj)}
 #' is called.
-#' @param order_medians logical. Whether or not to order the points by their 
+#' @param order_medians logical. Whether or not to order the points by their
 #' medians.
-#' @param horizontal logical. Whether or not you would like the lines to be 
-#' horizontal    
-#' 
+#' @param horizontal logical. Whether or not you would like the lines to be
+#' horizontal
+#'
 #' @details Points plot the simulations' medians, thin lines represent the 95%
-#' credibility intervals, and thick lines represent the 90% credibility 
+#' credibility intervals, and thick lines represent the 90% credibility
 #' intervals.
 
-stan_catterpillar <- function(obj, params, params_labels = NULL, 
+stan_catterpillar <- function(obj, params, params_labels = NULL,
                               order_medians = TRUE, horizontal = TRUE){
     # Load packages
     library(rstan)
     library(reshape2)
     library(dplyr)
     library(ggplot2)
-    
+
     # Extract all simulations
     sims <- as.data.frame(obj)
-    
+
     # Extract only desired parameters
-    names <- names(sims) 
-    sims_subset <- sims[, names %in% grep(pattern = params, x = names, 
+    names <- names(sims)
+    sims_subset <- sims[, names %in% grep(pattern = params, x = names,
                                           value = TRUE)]
 
     if (ncol(sims_subset) == 0) {
@@ -37,11 +37,11 @@ stan_catterpillar <- function(obj, params, params_labels = NULL,
     }
     # Melt for plotting
     sims_subset_molten <- suppressMessages(melt(sims_subset))
-    
+
     # Add labels
     if (!is.null(params_labels)) {
         message("\nEnsure that your parameter labels are in the same order as the parameters.\n")
-        if (length(params_labels) != 
+        if (length(params_labels) !=
                 length(unique(sims_subset_molten$variable))) {
             stop("params_labels must equal the number of plotted parameters.",
                 call. = FALSE)
@@ -49,7 +49,7 @@ stan_catterpillar <- function(obj, params, params_labels = NULL,
         sims_subset_molten$variable <- factor(sims_subset_molten$variable,
                                               labels = params_labels)
     }
-    
+
     # Find median and CI
     sims_subset_molten <- group_by(sims_subset_molten, variable)
     medians <- summarise(sims_subset_molten, median(value))
@@ -62,35 +62,35 @@ stan_catterpillar <- function(obj, params, params_labels = NULL,
     comb <- suppressMessages(inner_join(comb, upper90))
     comb <- suppressMessages(inner_join(comb, upper95))
     names(comb) <- c('params', 'lower95', 'lower90', 'medians', 'upper90',
-                     'upper95')   
-    
+                     'upper95')
+
     # Plot
     if (isTRUE(order_medians)){
-        pp <- ggplot(comb, aes(x = medians, y = reorder(params, medians), 
-                            xmin = lower95, 
+        pp <- ggplot(comb, aes(x = medians, y = reorder(params, medians),
+                            xmin = lower95,
                             xmax = upper95)) +
             geom_point(size = 3) +
-            geom_segment(aes(x = lower95, xend = upper95, 
+            geom_segment(aes(x = lower95, xend = upper95,
                             yend = reorder(params, medians)), size = 0.5) +
-            geom_segment(aes(x = lower90, xend = upper90, 
+            geom_segment(aes(x = lower90, xend = upper90,
                             yend = reorder(params, medians)), size = 1.5) +
             xlab('') + ylab('') +
             theme_bw()
     }
     else {
-        pp <- ggplot(comb, aes(x = medians, y = params, 
-                               xmin = lower95, 
+        pp <- ggplot(comb, aes(x = medians, y = params,
+                               xmin = lower95,
                                xmax = upper95)) +
             geom_point(size = 3) +
-            geom_segment(aes(x = lower95, xend = upper95, yend = params), 
+            geom_segment(aes(x = lower95, xend = upper95, yend = params),
                             size = 0.5) +
-            geom_segment(aes(x = lower90, xend = upper90, 
+            geom_segment(aes(x = lower90, xend = upper90,
                             yend = params), size = 1.5) +
             xlab('') + ylab('') +
             theme_bw()
     }
-    
+
     if (!isTRUE(horizontal)) pp <- pp + coord_flip()
-    
+
     return(pp)
 }
