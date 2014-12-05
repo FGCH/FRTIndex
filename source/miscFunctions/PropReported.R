4ccc33e3e37cd0844f3f4f80318ee81de77c25d8
christophergandrud <christopher.gandrud@gmail.com>
2014-12-05 15:43:57 +0100

    minor

--- source/miscFunctions/PropReported.R
+++ source/miscFunctions/PropReported.R
@@ -4,6 +4,9 @@ PropReported <- function(data){
     vars <- grep('Rep_.*', names(data), value = TRUE)
     data$sum <- rowSums(data[, vars])
     data$FRT_PropReport <- data$sum/length(vars)
-    Out <- data[, c('country', 'iso2c', 'year', 'FRT_PropReport')]
+    if ('country' %in% names(data)){
+        Out <- data[, c('country', 'iso2c', 'year', 'FRT_PropReport')]
+    }
+    else Out <- data[, c('iso2c', 'year', 'FRT_PropReport')]
     return(Out)
 }
