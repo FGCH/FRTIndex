eaf67046854b957bab70ae70d283fb4ee3970757
christophergandrud <christopher.gandrud@gmail.com>
2014-12-10 16:33:18 +0100

    rework stan file structure

--- source/FRT_Stan_in_Parallel.R
+++ source/FRT_Stan_in_Parallel.R
@@ -58,66 +58,7 @@ BaseSub <-
 
 # ---------------------------------------------------------------------------- #
 #### Specify Model ####
-frt_code <- "
-    data {
-        int<lower=1> C;                // number of countries
-        int<lower=1> T;                // number of years
-        int<lower=1> K;                // number of items
-        int<lower=1> N;                // number of observations
-        int<lower=1> cc[N];            // country for observation n
-        int<lower=1> tt[N];            // time for observation n
-        int<lower=1,upper=K> kk[N];    // item for observation n
-        int<lower=0,upper=1> y[N];     // response for observation n
-    }
-
-    parameters {
-        real delta;                // mean transparency
-        vector[C] alpha1;          // initial alpha for t = 1 before recentering
-        matrix[C,T] alpha;         // transparency for c,t - mean
-        vector[K] beta;            // difficulty of item k
-        vector[K] log_gamma;       // discrimination of k
-
-        //// all scale parameters have an implicit half Cauchy prior ////
-        real<lower=0> sigma_alpha[C];     // scale of abilities, per country
-        real<lower=0> sigma_beta;         // scale of difficulties
-        real<lower=0> sigma_gamma;        // scale of log discrimination
-    }
-
-    transformed parameters {
-        //// recenters transparency for t = 1 ////
-        vector[C] recentered_alpha1;
-        real mean_alpha1;
-        real<lower=0> sd_alpha1;
-
-        mean_alpha1 <- mean(alpha1);
-        sd_alpha1 <- sd(alpha1);
-        for (c in 1:C)
-        recentered_alpha1[c] <- ( alpha1[c] - mean_alpha1 ) / sd_alpha1;
-    }
-
-    model {
-        alpha1 ~ normal(0,1);   // informed constraints on the ability
-        // numerical issues with larger sd
-        for (c in 1:C) {
-            alpha[c,1] ~ normal(recentered_alpha1[c], 0.001);   // horrible hack
-            for (t in 2:T)
-            alpha[c,t] ~ normal(alpha[c,t-1], sigma_alpha[c]);
-        }
-
-        beta ~ normal(0,sigma_beta);
-        log_gamma ~ normal(0,sigma_gamma);
-        delta ~ cauchy(0,0.25);
-
-        sigma_alpha ~ cauchy(0,0.25);
-        sigma_beta ~ cauchy(0,0.25);
-        sigma_gamma ~ cauchy(0,0.25);
-
-        for (n in 1:N)
-        y[n] ~ bernoulli_logit(
-            exp(log_gamma[kk[n]])
-            * (alpha[cc[n],tt[n]] - beta[kk[n]] + delta) );
-    }
-"
+frt_code <- 
 
 #### Create data list for Stan ####
 frt_data <- list(
--- source/FRT_Stan_v_02_1990.R
+++ source/FRT_Stan_v_02_1990.R
@@ -2,7 +2,7 @@
 # FRT Index (using Stan)
 # Using only variables that are reported back to 1990
 # Christopher Gandrud
-# 8 December 2014
+# 10 December 2014
 # MIT License
 ################################################################################
 
@@ -88,67 +88,8 @@ MoltenBase$variable <- as.numeric(as.factor(MoltenBase$variable))
 MoltenReady <- arrange(MoltenBase, countrynum, yearnum, variable)
 
 # ---------------------------------------------------------------------------- #
-#### Specify Model ####
-frt_code <- "
-    data {
-        int<lower=1> C;                // number of countries
-        int<lower=1> T;                // number of years
-        int<lower=1> K;                // number of items
-        int<lower=1> N;                // number of observations
-        int<lower=1> cc[N];            // country for observation n
-        int<lower=1> tt[N];            // time for observation n
-        int<lower=1,upper=K> kk[N];    // item for observation n
-        int<lower=0,upper=1> y[N];     // response for observation n
-    }
-
-    parameters {
-        real delta;                // mean transparency
-        vector[C] alpha1;          // initial alpha for t = 1 before recentering
-        matrix[C,T] alpha;         // transparency for c,t - mean
-        vector[K] beta;            // difficulty of item k
-        vector[K] log_gamma;       // discrimination of k
-
-        //// all scale parameters have an implicit half Cauchy prior ////
-        real<lower=0> sigma_alpha[C];     // scale of abilities, per country
-        real<lower=0> sigma_beta;         // scale of difficulties
-        real<lower=0> sigma_gamma;        // scale of log discrimination
-    }
-
-    transformed parameters {
-        //// recenters transparency for t = 1 ////
-        vector[C] recentered_alpha1;
-        real mean_alpha1;
-        real<lower=0> sd_alpha1;
-
-        mean_alpha1 <- mean(alpha1);
-        sd_alpha1 <- sd(alpha1);
-        for (c in 1:C)
-            recentered_alpha1[c] <- ( alpha1[c] - mean_alpha1 ) / sd_alpha1;
-    }
-
-    model {
-        alpha1 ~ normal(0,1);   // informed constraints on the ability
-                                // numerical issues with larger sd
-        for (c in 1:C) {
-            alpha[c,1] ~ normal(recentered_alpha1[c], 0.001);   // horrible hack
-            for (t in 2:T)
-                alpha[c,t] ~ normal(alpha[c,t-1], sigma_alpha[c]);
-        }
-
-        beta ~ normal(0,sigma_beta);
-        log_gamma ~ normal(0,sigma_gamma);
-        delta ~ cauchy(0,0.25);
-
-        sigma_alpha ~ cauchy(0,0.25);
-        sigma_beta ~ cauchy(0,0.25);
-        sigma_gamma ~ cauchy(0,0.25);
-
-        for (n in 1:N)
-            y[n] ~ bernoulli_logit(
-                    exp(log_gamma[kk[n]])
-                    * (alpha[cc[n],tt[n]] - beta[kk[n]] + delta) );
-    }
-"
+#### Load Model ####
+frt_code <- 'source/FRT.stan'
 
 #### Create data list for Stan ####
 frt_data <- list(
@@ -163,8 +104,8 @@ frt_data <- list(
 )
 
 ##### Run model ####
-fit_NonIndp <- stan(model_code = frt_code, data = frt_data,
-                    iter = 2000, chains = 4,
+fit_NonIndp <- stan(file = 'source/FRT.stan', data = frt_data,
+                    iter = 50, chains = 4,
                     pars = c('delta', 'alpha', 'beta', 'log_gamma'),
                     diagnostic_file = paste0(
                         'modelOut/frt_sims_diagnostic', Sys.Date()))
