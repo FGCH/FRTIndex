20208e915eca47ece35441432398bb76555756ba
christophergandrud <christopher.gandrud@gmail.com>
2015-03-20 11:36:27 +0100

    minor

--- source/FRT_Stan_in_Parallel.R
+++ source/FRT_Stan_in_Parallel.R
@@ -81,7 +81,7 @@ sflist <-
     mclapply(1:4, mc.cores = 4,
         function(i) stan(fit = empty_stan, data = frt_data,
                         seed = i, chains = 1,
-                        iter = 50, chain_id = i,
+                        iter = 1000, chain_id = i,
                         pars = c('delta', 'alpha', 'beta', 'log_gamma'),
                         diagnostic_file = paste0(
                             'frt_sims_diagnostic', Sys.Date())
