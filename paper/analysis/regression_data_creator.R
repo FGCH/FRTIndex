8acae79339a2b160453ed889f6b2520e5a824d7b
christophergandrud <christopher.gandrud@gmail.com>
2016-04-14 17:26:02 +0200

    more central gov debt

--- paper/analysis/regression_data_creator.R
+++ paper/analysis/regression_data_creator.R
@@ -1,5 +1,3 @@
-#comb <- import('paper/analysis/frt0526.csv')
-
 # ---------------------------------------------------------------------------- #
 # Regression data set creator for FRT bond spreads paper
 # Christopher Gandrud
@@ -14,6 +12,7 @@ library(countrycode)
 library(lubridate)
 library(DataCombine)
 library(tidyr)
+library(fredr) # if not installed use devtools::github_install('christophergandrud/fredr')
 
 # Set working directory. Change as needed. --------
 setwd('/git_repositories/FRTIndex/')
@@ -21,9 +20,14 @@ setwd('/git_repositories/FRTIndex/')
 # Bond yields, spreads, and coefficient of variation --------------
 source('paper/analysis/bond_data_creator.R')
 
-# Load frt -------------------
+# Load frt (v2016) -------------------
 frt <- import('IndexData/FRTIndex_v2.csv')
 frt <- frt %>% select(iso2c, year, median) %>% rename(frt = median)
+
+# Load frt (v2015) -------------------
+frt_2015 <- import('IndexData/FRTIndex.csv')
+frt_2015 <- frt_2015 %>% select(iso2c, year, median) %>% 
+    rename(frt_2015 = median)
 
 # Load hrv ----------------
 hrv <- import('source/Hollyer_et_al_Compare/hrv_means.csv')
@@ -35,7 +39,7 @@ indicators <- c('GC.DOD.TOTL.GD.ZS', 'FP.CPI.TOTL.ZG', 'NY.GDP.MKTP.KD.ZG',
 wdi <- WDI(indicator = indicators, start = 1989, end = max_year, extra = T)
 
 wdi <- wdi %>%
-        rename(pubdebtgdp = GC.DOD.TOTL.GD.ZS) %>%
+        rename(pubdebtgdp_cent_wdi = GC.DOD.TOTL.GD.ZS) %>%
         rename(infl = FP.CPI.TOTL.ZG) %>%
         rename(cgdpgrowth = NY.GDP.MKTP.KD.ZG) %>%
         rename(pcgdp2005l = NY.GDP.PCAP.KD)
@@ -45,37 +49,107 @@ oecd <- subset(wdi, income == 'High income: OECD')
 oecd_growth <- oecd %>% group_by(year) %>%
                 summarize(oecdgrowth = mean(cgdpgrowth, na.rm = T))
 
-wdi <- wdi %>% select(iso2c, year, pubdebtgdp, infl, cgdpgrowth,
+# Extract central government debt and correct likely coding error
+wdi_debt <- wdi[, c('iso2c', 'year', 'pubdebtgdp_cent_wdi')]
+wdi_debt$pubdebtgdp_cent_wdi[wdi_debt$pubdebtgdp_cent_wdi >= 5102.68132] <- NA
+
+
+wdi <- wdi %>% select(iso2c, year, infl, cgdpgrowth,
                       pcgdp2005l, income)
 
 wdi <- merge(wdi, oecd_growth, by = 'year')
 
+## FRED central government debt -----------------
+debt_fred <- fred_loop(prefix = 'DEBTTL', suffix = 'A188A', 
+                       iso2c = unique(frt$iso2c),
+                       var_name = 'pubdebtgdp_cent_fred')
+
+debt_fred$year <- year(debt_fred$date)
+debt_fred <- debt_fred %>% select(-date)
 
 
+## Eurostat central government debt -------------
+# Downloaded from http://ec.europa.eu/eurostat/data/database
+# ESA 1995 consolidated debt
+eurostat <- import('paper/analysis/data_and_misc/gov_q_ggdebt_1_Data.csv',
+                   na.string = ':')
+
+# Keep only central government debt % of GDP
+eurostat <- eurostat %>% filter(SECTOR == 'Central government') %>%
+                filter(UNIT == "Percentage of gross domestic product (GDP)") %>%
+                filter(INDIC_NA == 'Government consolidated gross debt') %>%
+                select(-UNIT, -INDIC_NA, -SECTOR) 
+
+eurostat$year <- as.numeric(gsub('Q.', '', eurostat$TIME))
+eurostat$Value <- as.numeric(eurostat$Value)
+
+debt_eurostat <- eurostat %>% group_by(GEO, year) %>%
+                summarise(pubdebtgdp_cent_eu = mean(Value, na.rm = TRUE)) %>%
+                as.data.frame
+
+debt_eurostat$iso2c <- countrycode(debt_eurostat$GEO, origin = 'country.name',
+                             destination = 'iso2c')
+debt_eurostat <- DropNA(debt_eurostat, 'iso2c') %>% select(-GEO)
 
 ## IMF Historical Public Debt Data ---------------
-# From: https://www.imf.org/external/pubs/ft/weo/2015/02/weodata/download.aspx
-hist_pubdebtgdp <- import('paper/analysis/data_and_misc/WEOOct2015all.csv',
-                          na.string = 'n/a', header = TRUE)
+# From: https://www.imf.org/external/pubs/cat/longres.aspx?sk=24332.0
+hist_pubdebtgdp <- import('paper/analysis/data_and_misc/Debt Database Fall 2013 Vintage.xlsx',
+                          sheet = 2)
+hist_pubdebtgdp <- hist_pubdebtgdp[, c(1, 4:324)]
+hist_pubdebtgdp <- hist_pubdebtgdp %>% gather(year, pubdebtgdp_gen_imf, 
+                                              2:ncol(hist_pubdebtgdp))
 
-hist_pubdebtgdp <- hist_pubdebtgdp %>% filter(`WEO Subject Code` == 'GGXWDG_NGDP')
-
-hist_pubdebtgdp <- hist_pubdebtgdp[, c(2, 11:44)] 
-hist_pubdebtgdp <- hist_pubdebtgdp %>%
-    gather(year, pubdebtgdp, 2:ncol(hist_pubdebtgdp))
-
-hist_pubdebtgdp$iso2c <- countrycode(hist_pubdebtgdp$ISO, 
-                                     origin = 'iso3c', 
+hist_pubdebtgdp$iso2c <- countrycode(hist_pubdebtgdp$country, 
+                                     origin = 'country.name', 
                                      destination = 'iso2c')
 
 hist_pubdebtgdp[, 2:3] <- sapply(hist_pubdebtgdp[, 2:3], as.numeric)
 hist_pubdebtgdp <- hist_pubdebtgdp %>% filter(year >= 1989) %>% 
-    arrange(iso2c, year) %>% select(-ISO)
-hist_pubdebtgdp <- hist_pubdebtgdp %>% DropNA('iso2c')
+    arrange(country, year) %>% select(-country)
+
+
+## WEO Public Debt Data ---------------
+# From: https://www.imf.org/external/pubs/ft/weo/2015/02/weodata/download.aspx
+weo_debt <- import('paper/analysis/data_and_misc/WEOOct2015all.csv',
+                          na.string = 'n/a', header = TRUE)
+
+weo_debt <- weo_debt %>% filter(`WEO Subject Code` == 'GGXWDG_NGDP')
+
+weo_debt <- weo_debt[, c(2, 11:44)] 
+weo_debt <- weo_debt %>% gather(year, pubdebtgdp_gen_weo, 2:ncol(weo_debt))
+
+weo_debt$iso2c <- countrycode(weo_debt$ISO, 
+                                     origin = 'iso3c', 
+                                     destination = 'iso2c')
+
+weo_debt[, 2:3] <- sapply(weo_debt[, 2:3], as.numeric)
+weo_debt <- weo_debt %>% filter(year >= 1989) %>% arrange(iso2c, year) %>% 
+                    select(-ISO)
+weo_debt <- weo_debt %>% DropNA('iso2c')
+
+
+# Create debt measures data frame
+debt_comb <- merge(debt_eurostat, wdi_debt, by = c('iso2c', 'year'), 
+                   all = TRUE)
+debt_comb <- merge(debt_comb, debt_fred, by = c('iso2c', 'year'), 
+                   all = TRUE)
+debt_comb <- merge(debt_comb, hist_pubdebtgdp, by = c('iso2c', 'year'), 
+                   all = TRUE)
+debt_comb <- merge(debt_comb, weo_debt, by = c('iso2c', 'year'), 
+                   all = TRUE)
+
+debt_comb <- subset(debt_comb, year > 1988)
+debt_comb <- DropNA(debt_comb, 'iso2c')
+
+debt_comb$pubdebtgdp <- debt_comb$pubdebtgdp_cent_wdi
 
 # Fill in Missing values of public debt
-wdi <- FillIn(D1 = wdi, D2 = hist_pubdebtgdp, Var1 = 'pubdebtgdp', 
-                          Var2 = 'pubdebtgdp')
+debt_comb <- FillIn(D1 = debt_comb, D2 = debt_fred, Var1 = 'pubdebtgdp', 
+                          Var2 = 'pubdebtgdp_cent_fred')
+
+debt_comb <- FillIn(D1 = debt_comb, D2 = debt_eurostat, 
+                    Var1 = 'pubdebtgdp', 
+                    Var2 = 'pubdebtgdp_cent_eu')
 
 FindDups(wdi, c('iso2c', 'year'))
 
@@ -116,8 +190,10 @@ fiscal_trans <- fiscal_trans %>% gather(year, fiscal_trans_gfs,
 fiscal_trans$year <- fiscal_trans$year %>% as.numeric
 
 # Combine ------------
-comb <- merge(frt, hrv, by = c('iso2c', 'year'), all.x = T)
+comb <- merge(frt, frt_2015, by = c('iso2c', 'year'), all.x = T)
+comb <- merge(comb, hrv, by = c('iso2c', 'year'), all.x = T)
 comb <- merge(comb, bonds, by = c('iso2c', 'year'), all.x = T)
+comb <- merge(comb, debt_comb, by = c('iso2c', 'year'), all.x = T)
 comb <- merge(comb, wdi, by = c('iso2c', 'year'), all.x = T)
 comb <- merge(comb, fred_iv, by = 'year', all.x = T)
 comb <- merge(comb, fiscal_trans, by = c('iso2c', 'year'), all.x = T)
